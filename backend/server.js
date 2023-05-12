64eaced6dda9f4339066d2e0f07ce8551824a8db
ShahidBaig <siBaig@gmail.com>
2023-05-12 20:21:58 +0500

    Server updated for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 main().catch(err => console.log(err));
 async function main() {
-  await mongoose.connect(process.env.ConnectionString);
+  await mongoose.connect('mongodb+srv://KH_Saif:FqYEzIDpQ7TFTTNp@cluster0.fuxla.mongodb.net/NFT-Marketplace-ERC721?retryWrites=true'); //process.env.ConnectionString);
   console.log('database connected');
 }
 
@@ -14,6 +14,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/images', express.static('imgs'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "artnft.netlify.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const routes = require('./routes')(app);
 
 const server = app.listen('3333', () => {
