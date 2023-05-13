03f3bb7e9e009488c97fcb3cdf53311cfc7baa12
ShahidBaig <siBaig@gmail.com>
2023-05-13 21:11:40 +0500

    Node Server: remove arweave

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:3333/' // || 'https://nftserver.onrender.com/'
+  baseURL: process.env.API_URL || 'https://nftserver.onrender.com/'
+  // baseURL: process.env.API_URL || 'http://localhost:3333/'
 });
